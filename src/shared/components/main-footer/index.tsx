042edfc62c7bc80c64d047a49562440df68fb415
Lucas eduardo ⚡ <lucas_tranpo@YAHOO.com.br>
2025-04-13 23:10:59 -0300

    Atualização link rodapé

--- src/shared/components/main-footer/index.tsx
+++ src/shared/components/main-footer/index.tsx
@@ -15,7 +15,7 @@ export default function MainFooter() {
       <ul className="flex gap-2.5">
       <li>
           <a
-            href="https://portifolio-7a8a2c.netlify.app"
+            href="https://portfolio-2025-lucas.netlify.app"
             target="_blank"
             rel="noopener noreferrer"
           >
